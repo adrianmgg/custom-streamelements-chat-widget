b4e402b490ae84174b138b5e910165dea71b30f3
amgg <16143061+adrianmgg@users.noreply.github.com>
2022-01-09 17:32:39 -0800

    build script, typescript, rollup, gitignore
    add build.js script to be run with node
    add dev dependency of rollup
    add dev dependency of typescript
    change js file to typescript
    move files to src/ subdirectory
    add a basic gitignore

---
+++ build.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const rollup = require('rollup');
+const rollup_ts = require('@rollup/plugin-typescript');
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+
+function standard_prebuild({infile, outfile}) {
+	fsPromises.mkdir( path.dirname(outfile), {recursive: true} );
+}
+
+const build_copyfile = async ({infile, outfile}) => {
+	await standard_prebuild({infile, outfile});
+	await fsPromises.copyFile(infile, outfile);
+	console.log(`${infile} -> ${outfile}`);
+};
+
+function build_rollup(input_cfg, output_cfg) {
+	return async ({infile, outfile}) => {
+		await standard_prebuild({infile, outfile});
+		const bundle = await rollup.rollup({...input_cfg, input: infile});
+		await bundle.write({...output_cfg, file: outfile});
+		await bundle.close();
+		console.log(`${infile} -> ${outfile}`);
+	};
+}
+
+const clean_deletefile = ({outfile}) => fsPromises.unlink(outfile);
+
+const targets = [
+	{
+		infile: './src/ts/main.ts',
+		outfile: './build/js.js',
+		build: build_rollup({plugins: [rollup_ts()]}, {format: 'es'}), // not cached b/c depends on other files and i havent implemented that yet
+		clean: clean_deletefile,
+	},
+	...(['css.css', 'html.html', 'fields.json', 'data.json'].map(f=>({
+		infile: `./src/${f}`,
+		outfile: `./build/${f}`,
+		build: build_copyfile,
+		clean: clean_deletefile,
+	}))),
+];
+
+const run_task = (task) => Promise.all( targets.map( target=>target?.[task]?.(target) ) );
+
+run_task('build');
+
+
